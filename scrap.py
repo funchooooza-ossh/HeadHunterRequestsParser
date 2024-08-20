15a1474b6bca7d202299082781fb13fe67f868c7
funchooooza-ossh <uroddaunich@gmail.com>
2024-08-20 11:50:07 +1000

    Reworked scrap and hrefs methods
    Scrap method now is more quality-oriented. It's not passing vacancy-url if smth going wrong. It's retrying and only after n-count retries just skip this url.

--- scrap.py
+++ scrap.py
@@ -1,142 +1,147 @@
-#code by funchooooza-ossh Good luck ;)
+# code by funchooooza-ossh Good luck ;)
+import time
 import requests
 import fake_useragent
 import random
 import os
-import json
-import codecs
 from bs4 import BeautifulSoup as bs
 from dotenv import load_dotenv
 from pathlib import Path
-from lxml import etree
 from database import db
 
 
 class Scrapper:
     def url_list(baseurl, page_count):
-        if baseurl.endswith('&page='):
+        if baseurl.endswith("&page="):
             pass
 
-        elif '&page=' in baseurl:
-            parts=baseurl.split('&page=')
+        elif "&page=" in baseurl:
+            parts = baseurl.split("&page=")
             if parts[-1].isdigit():
-                baseurl=baseurl[:-1]
-                    
+                baseurl = baseurl[:-1]
+
         else:
-             baseurl=baseurl+'&page='
+            baseurl = baseurl + "&page="
 
         url_list = []
-        for page in range(0,page_count):
+        for page in range(0, page_count):
             url = baseurl
-            try:
-                url_list.append(f'{url}{page}')
-            except: break
+            url_list.append(f"{url}{page}")
 
-        return(url_list)
-
+        return url_list
 
     def page_count(soup):
-        block = soup.find('main')
-    
+        block = soup.find("main")
+
         if block:
-            pager = block.find('div', class_='pager')
-        
+            pager = block.find("div", class_="pager")
+
         if pager:
             pages_text = pager.get_text(strip=True)
-            
-            if 'дальше' in pages_text:
-                pages_text = pages_text.split('дальше')[0]
-                
-            if '...' in pages_text:
-                pages_text = pages_text.split('...')[-1]
+            if "дальше" in pages_text:
+                pages_text = pages_text.split("дальше")[0]
+            if "..." in pages_text:
+                pages_text = pages_text.split("...")[-1]
             else:
                 pages_text = pages_text[-1]
-            
             try:
                 pages = int(pages_text)
                 return pages
             except ValueError:
                 pass
-
         return 1
-    
-    
-    def hrefs(url,session,headers):
-        rs  = session.get(url,headers=headers).text
-        soup = bs(rs,'html.parser')
-        block = soup.find('main')
-        cards = block.find_all('h2')
 
-        href_list = []
-        for card in cards:
-            href=card.find('span')
-            href = href.find('a')
-            href_list.append(href['href'])
-        return href_list
-
-
-    def scrap(url):
-        session = requests.Session()
-        user=fake_useragent.FakeUserAgent().random
-        headers = {
-
-        'User-Agent':user
-
-        }
-        dotenv_path = Path('.','.env')
-        load_dotenv(dotenv_path)
-        proxylist = [
-        os.getenv('PROXY1'),
-        os.getenv('PROXY2'),
-        os.getenv('PROXY3')
-        ]
-        proxy=random.choice(proxylist)
-        
-
-        proxies = {
-        'http':proxy,
-        'https':proxy
-        }
-        rs = session.get(url,headers=headers,proxies=proxies).text
-        soup = bs(rs,'html.parser')
-        try:
-            vacancy = soup.find(attrs={'data-qa': 'vacancy-title'}).get_text() 
-            vacancy = Scrapper.prettify(vacancy)
-
+    def hrefs(url, session, headers, max_retries, retry_delay):
+        attempts = 0
+        while attempts < max_retries:
             try:
-                salary = soup.find(attrs={'data-qa': 'vacancy-salary'}).get_text()
-                salary = Scrapper.prettify(salary)
-                
-            except: salary = "Salary not presented"
+                rs = session.get(url, headers=headers)
+                rs.raise_for_status()  # проверка статуса ответа
+                soup = bs(rs.text, "lxml")
 
-            company =  soup.find(attrs={'data-qa': 'vacancy-company-name'}).get_text()
-            company = Scrapper.prettify(company)
-            
-    
+                block = soup.find("main")
+                if not block:
+                    raise ValueError("Не удалось найти блок c вакансиями на странице")
 
-            location_dict = {
-                'data-qa': ["vacancy-view-location","vacancy-view-raw-address"]
-            }
+                cards = block.find_all("h2")
+                href_list = []
+                for card in cards:
+                    href = card.find("span")
+                    if href:
+                        href = href.find("a")
+                        if href and "href" in href.attrs:
+                            href_list.append(href["href"])
+                        else:
+                            raise ValueError("Не удалось найти ссылку в элементе")
+                    else:
+                        raise ValueError("Не удалось найти элемент 'span'")
 
-            for value in location_dict['data-qa']:
-                location = soup.find(attrs={'data-qa': value})
-    
-                if location:
-                    location = location.text
-                    location = Scrapper.prettify(location)
-                    break
-                else: continue
-       
-            print(url)
+                return href_list
+            except Exception as e:
+                print(f"Попытка {attempts + 1} из {max_retries} не удалась: {e}")
+                attempts += 1
+                if attempts < max_retries:
+                    time.sleep(retry_delay)
 
-            description = soup.find(attrs={'data-qa': 'vacancy-description'}).get_text(strip=True)
-            description = Scrapper.prettify(description)
-            
-            db.inputdata(vacancy, salary, company, location, description, url)
-            
-        except: pass
-               
-                    
+        raise Exception(f"Не удалось получить ссылки после {max_retries} попыток")
+
+    def scrap(url, max_retries=3, retry_delay=1):
+        session = requests.Session()
+        user = fake_useragent.FakeUserAgent().random
+        headers = {"User-Agent": user}
+        dotenv_path = Path(".", ".env")
+        load_dotenv(dotenv_path)
+        proxylist = [os.getenv("PROXY1"), os.getenv("PROXY2"), os.getenv("PROXY3")]
+        proxy = random.choice(proxylist)
+
+        proxies = {"http": proxy, "https": proxy}
+        for attempt in range(max_retries):
+            try:
+                rs = session.get(url, headers=headers, proxies=proxies)
+                rs.raise_for_status()
+                soup = bs(rs.text, "lxml")
+
+                vacancy = soup.find(attrs={"data-qa": "vacancy-title"}).get_text()
+                vacancy = Scrapper.prettify(vacancy)
+
+                try:
+                    salary = soup.find(attrs={"data-qa": "vacancy-salary"}).get_text()
+                    salary = Scrapper.prettify(salary)
+                except:
+                    salary = "Salary not presented"
+
+                company = soup.find(
+                    attrs={"data-qa": "vacancy-company-name"}
+                ).get_text()
+                company = Scrapper.prettify(company)
+
+                location = None
+                location_qa_list = ["vacancy-view-location", "vacancy-view-raw-address"]
+                for qa in location_qa_list:
+                    location_elem = soup.find(attrs={"data-qa": qa})
+                    if location_elem:
+                        location = Scrapper.prettify(location_elem.text)
+                        break
+                if not location:
+                    location = "Location not specified"
+
+                description = soup.find(
+                    attrs={"data-qa": "vacancy-description"}
+                ).get_text(strip=True)
+                description = Scrapper.prettify(description)
+
+                db.inputdata(vacancy, salary, company, location, description, url)
+
+                print(f"Data fetched successfully from {url}")
+                break
+            except Exception as e:
+                print(f"Error fetching data from {url}: {e}")
+                if attempt < max_retries - 1:
+                    print(f"Retrying... ({attempt+2}/{max_retries})")
+                    time.sleep(retry_delay)
+                else:
+                    print("Max retries reached. Moving to the next URL")
+
     def prettify(smth):
-        smth=smth.replace("'","")
+        smth = smth.replace("'", "")
         return smth
